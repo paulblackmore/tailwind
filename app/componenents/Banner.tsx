79eb89cc484c8a4c9ef9c8e4e18ac36d5d27b29f
paul <paulmichaelblackmore@gmail.com>
2024-12-02 18:27:45 +0000

    chore: move Banner into own component

---
+++ app/componenents/Banner.tsx
@@ -0,0 +1,14 @@
+const Banner = () => {
+  return (
+    <div className="w-full h-80 bg-gradient-to-r from-cyan-500 to-blue-400 flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-4xl font-bold">
+          I'm just messing around with Tailwind CSS...
+        </h1>
+        <p>Some random finding in the world of Tailwind!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
