a03dd224af1e1f7063a7082b4cb96faf48e7502a
paul <paulmichaelblackmore@gmail.com>
2024-12-02 17:37:09 +0000

    feat: add banner with background gradient

--- app/componenents/Header.tsx
+++ app/componenents/Header.tsx
@@ -1,24 +1,24 @@
 import Link from "next/link";
 
 const Header = () => (
-  <header className="w-full h-auto bg-slate-600 p-6 text-white flex justify-between">
+  <header className="w-full h-auto bg-transparent p-6 text-white flex justify-between absolute top-0 z-10">
     <h1 className="font-bold">Tailwind</h1>
     <nav className="flex justify-end">
       <div className="flex justify-between w-60">
         <Link
-          className="text-yellow-300 font-semibold hover:text-yellow-500"
+          className="text-gray-50 font-semibold hover:text-gray-100"
           href="/"
         >
           Users
         </Link>
         <Link
-          className="text-yellow-300 font-semibold hover:text-yellow-500"
+          className="text-gray-50 font-semibold hover:text-gray-100"
           href="/"
         >
           Profile
         </Link>
         <Link
-          className="text-yellow-300 font-semibold hover:text-yellow-500"
+          className="text-gray-50 font-semibold hover:text-gray-100"
           href="/"
         >
           Settings
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import Image from "next/image";
+
 const Home = () => {
-  return <div>{/* <h1>Hello World!</h1> */}</div>;
+  return (
+    <section className="w-full h-80 absolute top-0 bg-gradient-to-r from-cyan-500 to-blue-400 flex justify-center items-center">
+      <h1 className="text-4xl font-bold">
+        I'm just messing around with Tailwind CSS...
+      </h1>
+    </section>
+  );
 };
 
 export default Home;
