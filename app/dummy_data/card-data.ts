0f312c30aace4dcc38f55917a7ff48da4c4dc408
paul <paulmichaelblackmore@gmail.com>
2024-12-03 14:54:08 +0000

    chore: add more dummy data

--- app/dummy_data/card-data.ts
+++ app/dummy_data/card-data.ts
@@ -37,4 +37,16 @@ export const cardData: CardType[] = [
     desc: " Ever man are put down his very. And marry may table him avoid. Hard sell it were into it upon. He forbade affixed parties of assured to me windows. Happiness him nor she disposing provision. Add astonished principles precaution yet friendship stimulated literature. State thing might stand one his plate. Offending or extremity therefore so difficult he on provision. Tended depart turned not are.",
     btnText: "Click to View",
   },
+  {
+    id: 7,
+    title: "Add astonished principles.",
+    desc: " Ever man are put down his very. And marry may table him avoid. Hard sell it were into it upon. He forbade affixed parties of assured to me windows. Happiness him nor she disposing provision. Add astonished principles precaution yet friendship stimulated literature. State thing might stand one his plate. Offending or extremity therefore so difficult he on provision. Tended depart turned not are.",
+    btnText: "Click to View",
+  },
+  {
+    id: 8,
+    title: "Add astonished principles.",
+    desc: " Ever man are put down his very. And marry may table him avoid. Hard sell it were into it upon. He forbade affixed parties of assured to me windows. Happiness him nor she disposing provision. Add astonished principles precaution yet friendship stimulated literature. State thing might stand one his plate. Offending or extremity therefore so difficult he on provision. Tended depart turned not are.",
+    btnText: "Click to View",
+  },
 ];
