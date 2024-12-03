ed5c5684b5bff11f32a4482b9a42c1d9319adbb3
paul <paulmichaelblackmore@gmail.com>
2024-12-03 14:48:42 +0000

    feat: add FB and Linkedin icons to footer

--- app/componenents/Footer.tsx
+++ app/componenents/Footer.tsx
@@ -1,7 +1,11 @@
-export const Footer = () => (
-  <footer className="bg-gradient-to-r from-blue-400 to-cyan-500 w-full flex justify-between p-6 mt-10">
-    <p>&copy; Paul Blackmore</p>
+import { FaFacebookSquare, FaLinkedin } from "react-icons/fa";
 
-    <div>{/* <div>Socials go here</div> */}</div>
+export const Footer = () => (
+  <footer className="bg-gradient-to-r from-blue-400 to-cyan-500 w-full flex justify-between items-center p-6 mt-10">
+    <p>&copy; Paul Blackmore</p>
+    <div className="flex gap-2">
+      <FaFacebookSquare />
+      <FaLinkedin />
+    </div>
   </footer>
 );
